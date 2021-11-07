99431b84071e15036b6c3fa3ca0043d5c1ae43c9
Amar Hodzic <amar_hodzic@hotmail.com>
2021-11-07 18:21:10 +0100

    Add signature var to shingle class

--- homework_1/shingle.py
+++ homework_1/shingle.py
@@ -1,13 +1,15 @@
 import re
+from min_hashing import min_hashing_signature
 
 
 class Shingle():
-    """Creates a set of hashed k-shingles from a text file for a given
-       file path and k
+    """Creates a set of hashed k-shingles with a signature of length n
+       from a text file
     """
-    def __init__(self, k: int, document_path: str) -> None:
-        shingle_set = set()
+    def __init__(self, k: int, index_permutation: list, document_path: str) -> None:
+        self.shingle_set = set()
 
+        # Open and clean the text
         with open(document_path, encoding='latin1') as f:
             txt = f.read()
         txt = re.sub(r'\n|\t', ' ', txt)
@@ -15,11 +17,13 @@ class Shingle():
 
         index_start = 0
         while(index_start + k < len(txt)):
-            shingle_set.add(txt[index_start:index_start + k])
+            self.shingle_set.add(txt[index_start:index_start + k])
             index_start += 1
-        self.set = set(sorted(map(hash, shingle_set)))
+        self.signature = min_hashing_signature(list(self.shingle_set),
+                                               index_permutation)
 
 
 if __name__ == '__main__':
-    shingle = Shingle(10, 'data/sport_articles/Text0001.txt')
+    shingle = Shingle(10, 100, 'data/sport_articles/Text0001.txt')
     print(shingle.set)
+    print(shingle.signature)
