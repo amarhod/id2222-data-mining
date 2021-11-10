363276777f7c064a09fc6b3e357e94c7e329c6ad
callmeGoldenboy <natanteferi@gmail.com>
2021-11-10 12:04:08 +0100

    fixed shingle

--- homework_1/shingle.py
+++ homework_1/shingle.py
@@ -1,29 +1,37 @@
 import re
 from min_hashing import min_hashing_signature
 
-
 class Shingle():
-    """Creates a set of hashed k-shingles with a signature of length n
-       from a text file
-    """
-    def __init__(self, k: int, index_permutation: list, document_path: str) -> None:
-        self.shingle_set = set()
+		"""Creates a set of hashed k-shingles with a signature of length n
+			 from a text file
+		"""
+		def __init__(self, k: int, document_path: str) -> None:
+				self.shingle_set = set()
+				self.mapping = {} #this is just in case we want to get the text from the shingle hash 
+				self.hashed_shingle_set = set()  
 
-        # Open and clean the text
-        with open(document_path, encoding='latin1') as f:
-            txt = f.read()
-        txt = re.sub(r'\n|\t', ' ', txt)
-        txt = re.sub(r'\s{2,}', ' ', txt)
+				# Open and clean the text
+				with open(document_path, encoding='latin1') as f:
+						txt = f.read()
+				txt = re.sub(r'\n|\t', ' ', txt)
+				txt = re.sub(r'\s{2,}', ' ', txt)
 
-        index_start = 0
-        while(index_start + k < len(txt)):
-            self.shingle_set.add(txt[index_start:index_start + k])
-            index_start += 1
-        self.signature = min_hashing_signature(list(self.shingle_set),
-                                               index_permutation)
+				index_start = 0
+				shingle_hash = 0
+				while(index_start + k <= len(txt)):
+						shingle = txt[index_start:index_start + k]
+						if shingle not in self.shingle_set:
+							self.shingle_set.add(shingle)
+							self.hashed_shingle_set.add(shingle_hash)
+							self.mapping[shingle_hash] = shingle
+							shingle_hash += 1
+						index_start += 1
 
 
 if __name__ == '__main__':
-    shingle = Shingle(10, 100, 'data/sport_articles/Text0001.txt')
-    print(shingle.set)
-    print(shingle.signature)
+		shingle = Shingle(5, 100, './tests/text_1.txt')
+		print(shingle.shingle_set)
+		print(shingle.hashed_shingle_set)
+		print(shingle.mapping)
+		assert len(shingle.shingle_set) == len(shingle.hashed_shingle_set)
+		#print(shingle.signature)
