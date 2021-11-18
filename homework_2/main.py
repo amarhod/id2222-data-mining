2f32fc1ed57324c846880ae8995196e26d357f73
Amar Hodzic <amar_hodzic@hotmail.com>
2021-11-18 13:06:11 +0100

    Enhance main

--- homework_2/main.py
+++ homework_2/main.py
@@ -1,19 +1,32 @@
+import argparse
 from frequent_itemsets import FrequentItemsets
 
 
-def read_basket_data(filepath='data/T10I4D100K.dat') -> list[list]:
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--itemset_size', nargs='?', default=2, type=int)
+    parser.add_argument('--number_of_baskets', nargs='?', default=100, type=int,
+                        help='number of baskets used from the data set')
+    parser.add_argument('--support', nargs='?', default=8, type=int)
+    return parser.parse_args()
+
+
+def read_basket_data(number_of_baskets: int, filepath='data/T10I4D100K.dat') -> list[list]:
     with open(filepath) as f:
         lines = f.readlines()
     baskets = []
-    for line in lines:
+    for i, line in enumerate(lines):
+        if i >= number_of_baskets:
+            break
         baskets.append(list(map(int, line.strip().split(' '))))
     return baskets
 
 
 def main():
-    baskets = read_basket_data()
-    frequent_itemssets = FrequentItemsets(baskets, 2, 10)
-    print(frequent_itemssets.itemsets)
+    args = get_args()
+    baskets = read_basket_data(args.number_of_baskets)
+    frequent_itemssets = FrequentItemsets(baskets, args.itemset_size, args.support)
+    print(f'Itemsets with support at or above {args.support}:', frequent_itemssets.itemsets)
 
 
 if __name__ == '__main__':
