4fa0a8adf7f1708e9e3a1827667a201b2983281d
Amar Hodzic <amar_hodzic@hotmail.com>
2021-11-21 19:12:49 +0100

    Improve frequent itemset finding

--- homework_2/frequent_itemsets.py
+++ homework_2/frequent_itemsets.py
@@ -6,52 +6,64 @@ class FrequentItemsets():
         self.baskets, self.k, self.support = baskets, k, support
         self.itemsets = self.find_frequent_itemsets(k)
 
-    def find_item_frequency(self) -> dict[str, int]:
-        """Finds frequency for singleton itemsets (k=1)
+    def find_frequent_items(self) -> dict[tuple, int]:
+        """Find frequency for singleton itemsets (k=1)
         """
         item_frequency = {}
         for basket in self.baskets:
             for item in basket:
-                item_count = item_frequency.get(str(item), 0)
-                item_frequency[str(item)] = item_count + 1
+                item_count = item_frequency.get((item,), 0)
+                item_frequency[(item,)] = item_count + 1
         return item_frequency
 
-    def find_frequent_itemsets(self, k: int) -> dict[str, int]:
-        """Finds frequency for itemsets with size k
+    def find_frequent_itemsets(self, k: int) -> dict[tuple, int]:
+        """Find frequency for k-itemsets by going through each transaction and checking there is support
+           for any of the possible k-itemsets.
+           This is done by creating k-sized combinations of the transaction and checking if any of them
+           are in our candidate k-itemsets. It is more efficiant than looping over all candidates and
+           finding support in the transaction.
         """
-        item_frequency = {}
+        itemset_frequency = {}
+        number_of_itemsets = 0
         for i in range(1, k + 1):
             if i == 1:
-                item_frequency = self.find_item_frequency()
-                item_frequency = filter_items_with_support(item_frequency, self.support)  # Remove items without support
+                itemset_frequency = self.find_frequent_items()
+                itemset_frequency = filter_items_with_support(itemset_frequency, self.support)
+                self.singletons = [item for item in itemset_frequency]
             else:
-                unique_items = get_unique_items_from_itemsets(item_frequency)
-                possible_itemsets = list(itertools.combinations(unique_items, i))
+                possible_itemsets = self.get_candidate_itemsets(itemset_frequency)
                 for basket in self.baskets:
-                    for possible_itemset in possible_itemsets:
-                        if set(possible_itemset) <= set(basket):  # Check if possible_itemset is a subset of the basket
-                            increment_frequency_for_itemset(item_frequency,
-                                                            '-'.join(map(str, sorted(possible_itemset))))
-                item_frequency = filter_items_with_support(item_frequency, self.support)
-        return item_frequency
+                    basket_itemsets = list(itertools.combinations(basket, i))
+                    for itemset in basket_itemsets:
+                        itemset = tuple(sorted(itemset))
+                        if itemset in possible_itemsets:  # Check if possible_itemset is a subset of the basket
+                            increment_frequency_for_itemset(itemset_frequency, itemset)
+
+                itemset_frequency = filter_items_with_support(itemset_frequency, self.support)
+
+            if len(itemset_frequency) <= number_of_itemsets:  # Stop searching if no new itemsets found for current k
+                break
+            else:
+                number_of_itemsets = len(itemset_frequency)
+        return itemset_frequency
+
+    def get_candidate_itemsets(self, itemset_frequency: dict[tuple, int]) -> set:
+        """Generate k-itemsets with supported k-1-itemsets and singletons
+        """
+        itemset_candidates = set()
+        for itemset in itemset_frequency:
+            for singleton in self.singletons:
+                if singleton not in itemset:
+                    itemset_candidates.add(tuple(sorted(itemset + singleton)))
+        return itemset_candidates
 
 
-def increment_frequency_for_itemset(item_frequency: dict[str, int], item: list[int]) -> None:
-    item_frequency[item] = item_frequency.get(item, 0) + 1
+def increment_frequency_for_itemset(itemset_frequency: dict[tuple, int], item: tuple) -> None:
+    itemset_frequency[item] = itemset_frequency.get(item, 0) + 1
 
 
-def filter_items_with_support(item_frequency: dict[str, int], support: int) -> dict[str, int]:
-    return {k: v for (k, v) in item_frequency.items() if v >= support}
-
-
-def get_unique_items_from_itemsets(item_frequency: dict[str, int]) -> list[int]:
-    """Creates a list of unique items from the itemsets (which are represented as strings)
-    """
-    unique_items = set()
-    for item_set in item_frequency.keys():
-        for item in map(int, item_set.split('-')):
-            unique_items.add(int(item))
-    return list(unique_items)
+def filter_items_with_support(itemset_frequency: dict[tuple, int], support: int) -> dict[tuple, int]:
+    return {k: v for (k, v) in itemset_frequency.items() if v >= support}
 
 
 if __name__ == '__main__':
