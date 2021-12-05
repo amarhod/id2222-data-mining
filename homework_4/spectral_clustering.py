8ec1f8583056a78f6304dce970cbe4c3037a7148
Amar Hodzic <amar_hodzic@hotmail.com>
2021-12-05 19:40:14 +0100

    Add type hints and fix edge zero indexing

--- homework_4/spectral_clustering.py
+++ homework_4/spectral_clustering.py
@@ -19,42 +19,42 @@ class SpectralClustering():
         self.Y = self.get_renormalized_matrix(self.X)
         self.cluster_labels = KMeans(n_clusters=k).fit(self.Y).labels_
 
-    def get_affinity_matrix(self):
-        matrix = np.zeros((self.edges.max() + 1, self.edges.max() + 1))
+    def get_affinity_matrix(self) -> np.ndarray:
+        matrix = np.zeros((self.edges.max(), self.edges.max()))
         for u, v in self.edges:
-            a = round(affinity(u, v), 2)
-            matrix[u, v] = a
-            matrix[v, u] = a
+            a = round(affinity(u-1, v-1), 2)
+            matrix[u-1, v-1] = a
+            matrix[v-1, u-1] = a
         return matrix
 
-    def get_k_eigenvectors(self, m, k):
+    def get_k_eigenvectors(self, m: np.ndarray, k: int) -> np.ndarray:
         _, eigenvectors = np.linalg.eigh(m)
         return eigenvectors[:, -k:]
 
-    def get_renormalized_matrix(self, m):
+    def get_renormalized_matrix(self, m: np.ndarray) -> np.ndarray:
         Y = np.zeros_like(m)
         for i, row in enumerate(m):
             divider = np.sqrt(np.dot(row, row.T))
             Y[i] = row / divider
         return Y
 
-    def get_adjacency_matrix(self):
+    def get_adjacency_matrix(self) -> np.ndarray:
         matrix = np.zeros((self.edges.max() + 1, self.edges.max() + 1))
         matrix[self.edges[:, 0], self.edges[:, 1]] = 1
         matrix[self.edges[:, 1], self.edges[:, 0]] = 1
         return matrix
 
 
-def affinity(u, v, sig=1):
+def affinity(u, v, sig=1) -> float:
     if u == v:
-        return 0
+        return 0.0
     a = math.pow(abs(u - v), 2)
     b = 2 * math.pow(sig, 2)
     return math.exp(- a / b)
 
 
 def main():
-    edge_list = [(0, 1), (0, 4), (1, 4), (2, 1), (2, 3), (3, 5), (4, 3)]
+    edge_list = [(1, 2), (1, 3), (2, 5), (3, 2), (3, 4), (4, 6), (5, 4)]
     SC = SpectralClustering(edge_list, 2)
     print(SC.A)
     print(SC.D)
