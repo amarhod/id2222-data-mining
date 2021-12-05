a4a63dc3bb828dc42f8e37eab9cc3b18b8695872
Amar Hodzic <amar_hodzic@hotmail.com>
2021-12-05 13:23:08 +0100

    Refactor into a class

--- homework_4/spectral_clustering.py
+++ homework_4/spectral_clustering.py
@@ -7,6 +7,29 @@ import math
 import numpy as np
 
 
+class SpectralClustering():
+    def __init__(self, edges: list, k: int) -> None:
+        self.edges = np.array(edges)
+        self.k = k
+        self.A = self.get_affinity_matrix()
+        self.D = np.diag(np.sum(self.A, axis=1))
+        self.eigenvalues = np.linalg.eig(self.A)
+
+    def get_affinity_matrix(self):
+        matrix = np.zeros((self.edges.max() + 1, self.edges.max() + 1))
+        for u, v in self.edges:
+            a = round(affinity(u, v), 2)
+            matrix[u, v] = a
+            matrix[v, u] = a
+        return matrix
+
+    def get_adjacency_matrix(self):
+        matrix = np.zeros((self.edges.max() + 1, self.edges.max() + 1))
+        matrix[self.edges[:, 0], self.edges[:, 1]] = 1
+        matrix[self.edges[:, 1], self.edges[:, 0]] = 1
+        return matrix
+
+
 def affinity(u, v, sig=1):
     if u == v:
         return 0
@@ -15,39 +38,12 @@ def affinity(u, v, sig=1):
     return math.exp(- a / b)
 
 
-def get_adjacency_matrix(edges: list):
-    edges = np.array(edges)
-    matrix = np.zeros((edges.max() + 1, edges.max() + 1))
-    matrix[edges[:, 0], edges[:, 1]] = 1
-    matrix[edges[:, 1], edges[:, 0]] = 1
-    return matrix
-
-
-def get_affinity_matrix(edges: list):
-    edges = np.array(edges)
-    matrix = np.zeros((edges.max() + 1, edges.max() + 1))
-    for u, v in edges:
-        a = round(affinity(u, v), 2)
-        matrix[u, v] = a
-        matrix[v, u] = a
-    return matrix
-
-
-def get_diagonal_matrix(affinity_matrix):
-    matrix = np.zeros(affinity_matrix.shape)
-    for i in range(matrix.shape[0]):
-        matrix[i, i] = affinity_matrix[i].sum()
-    return matrix
-
-
 def main():
     edge_list = [(0, 1), (0, 4), (1, 4), (2, 1), (2, 3), (3, 5), (4, 3)]
-    A = get_affinity_matrix(edge_list)
-    print(A)
-    D = get_diagonal_matrix(A)
-    print(D)
-    eigenvalues = np.linalg.eig(A)
-    print(eigenvalues)
+    SC = SpectralClustering(edge_list, 0)
+    print(SC.A)
+    print(SC.D)
+    print(SC.eigenvalues)
 
 
 if __name__ == '__main__':
