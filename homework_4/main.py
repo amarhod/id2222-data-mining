7850b20e8259c448ff141e6999036db95ac39e05
Amar Hodzic <amar_hodzic@hotmail.com>
2021-12-06 13:57:15 +0100

    Add graph plotting with networkx and matplotlib

--- homework_4/main.py
+++ homework_4/main.py
@@ -1,32 +1,27 @@
 from __future__ import annotations
 import argparse
+import matplotlib.pyplot as plt
+import networkx as nx
 from spectral_clustering import SpectralClustering
 
 
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--path', nargs='?', default='data/example1.dat', type=str)
-    parser.add_argument('--k', nargs='?', default=2, type=int)
+    parser.add_argument('--k', nargs='?', default=4, type=int)
     return parser.parse_args()
 
 
-def read_edge_data(filepath: str) -> list:
-    with open(filepath) as f:
-        lines = f.readlines()
-    edge_list = []
-    for i, line in enumerate(lines):
-        line_split = line.strip().split(',')
-        u = int(line_split[0]) - 1
-        v = int(line_split[1]) - 1
-        edge_list.append((u, v))
-    return edge_list
+def plot_graph_clustering(graph_data, labels: list[int]):
+    nx.draw(nx.Graph(graph_data), node_size=30, node_color=labels)
+    plt.show()
 
 
 def main():
     args = get_args()
-    edge_list = read_edge_data(args.path)
-    sc = SpectralClustering(edge_list, 3)
-    print(sc.cluster_labels)
+    graph_data = nx.read_edgelist(args.path, delimiter=',', nodetype=int)
+    sc = SpectralClustering(graph_data.edges, args.k)
+    plot_graph_clustering(graph_data, sc.cluster_labels)
 
 
 if __name__ == '__main__':
